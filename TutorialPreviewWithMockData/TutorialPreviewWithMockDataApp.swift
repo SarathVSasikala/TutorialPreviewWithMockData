82235e8f8df8a230dda64443a60e1129d882ff93
Sarath Sasikala <sarathsasikala@hotmail.com>
2023-10-12 14:39:42 +0100

    Initial Commit

---
+++ TutorialPreviewWithMockData/TutorialPreviewWithMockDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialPreviewWithMockDataApp.swift
+//  TutorialPreviewWithMockData
+//
+//  Created by Sarath Sasikala on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TutorialPreviewWithMockDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TutorialPreviewWithMockDataUITests/TutorialPreviewWithMockDataUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TutorialPreviewWithMockDataUITestsLaunchTests.swift
+//  TutorialPreviewWithMockDataUITests
+//
+//  Created by Sarath Sasikala on 12/10/2023.
+//
+
+import XCTest
+
+final class TutorialPreviewWithMockDataUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
